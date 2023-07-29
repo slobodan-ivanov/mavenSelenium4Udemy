b5abf50c6c32f56a1dd5987808cb827c11aa2d1b
slobodan-ivanov <67831879+slobodan-ivanov@users.noreply.github.com>
2023-07-29 15:47:23 +0200

    Selenium 4 - New locators - below, toLeftOf and toRightOf

--- src/test/java/com/kinto/qa/selenium4/NewLocatorsTest.java
+++ src/test/java/com/kinto/qa/selenium4/NewLocatorsTest.java
@@ -17,12 +17,22 @@ public class NewLocatorsTest extends BaseTest {
         driver.manage().window().maximize();
         driver.get("https://www.rahulshettyacademy.com/angularpractice/");
 
+        // .above()
         WebElement nameInput = driver.findElement(By.cssSelector("[name='name']"));
         String nameLabelText = driver.findElement(with(By.tagName("label")).above(nameInput)).getText();
         System.out.println("dateofBirth: " + nameLabelText);
 
+        // .below()
+        WebElement dateOfBirthLabel = driver.findElement(By.cssSelector("label[for='dateofBirth']"));
+        driver.findElement(with(By.tagName("input")).below(dateOfBirthLabel)).click();
 
+        // .toLeftOf()
+        WebElement labelCheckbox = driver.findElement(By.cssSelector("label[for='exampleCheck1']"));
+        driver.findElement(with(By.tagName("input")).toLeftOf(labelCheckbox)).click();
 
+        WebElement firstRadioButton = driver.findElement(By.id("inlineRadio1"));
+        String firstRadioButtonLabelText = driver.findElement(with(By.tagName("label")).toRightOf(firstRadioButton)).getText();
+        assertEquals("Student", firstRadioButtonLabelText);
 
     }
 
