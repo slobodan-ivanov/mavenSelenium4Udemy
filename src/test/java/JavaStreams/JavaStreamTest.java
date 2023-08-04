455704f67552c1b1d52ee3e1075b0edbca9ec2c0
slobodan-ivanov <67831879+slobodan-ivanov@users.noreply.github.com>
2023-08-04 20:02:38 +0200

    Java Streams - Stream map, concat and matches

--- src/test/java/JavaStreams/JavaStreamTest.java
+++ src/test/java/JavaStreams/JavaStreamTest.java
@@ -1,9 +1,12 @@
 package JavaStreams;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class JavaStreamTest {
 
@@ -47,13 +50,33 @@ public class JavaStreamTest {
                 s -> s.startsWith("A")).count(); // Nothing happens
 
         System.out.println("Print names which length is > 4");
-        names.stream().filter(s -> s.length() > 4).forEach(System.out::println);
+        names.stream().filter(s -> s.length() > 4).forEach(s -> System.out.println(s));
         System.out.println("---------------");
 
         // Limit by 1 element
         System.out.println("Print names which length is > 4");
-        names.stream().filter(s -> s.length() > 4).limit(1).forEach(System.out::println);
+        names.stream().filter(s -> s.length() > 4).limit(1).forEach(s -> System.out.println(s));
         System.out.println("---------------");
+    }
+
+    @Test
+    public void streamMapConcatMatch() {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Abuhar");
+        names.add("Boban");
+        names.add("Vanja");
+        names.add("Adam");
+        names.add("Mixa");
+
+        names.stream().filter(s -> s.endsWith("a")).map(s -> s.toUpperCase()).forEach(s -> System.out.println(s));
+
+        // print names starts with A and sort and uppercase
+        names.stream().filter(s -> s.endsWith("a")).map(s -> s.toUpperCase()).sorted().forEach(s -> System.out.println(s));
+
+        // Concat stream and return boolean if match with some word
+        Stream<String> newStream = Stream.concat(names.stream(), names.stream());
+        boolean anyMatch = newStream.anyMatch(s -> s.equals("Adam"));
+        assertTrue(anyMatch);
     }
 
 }
